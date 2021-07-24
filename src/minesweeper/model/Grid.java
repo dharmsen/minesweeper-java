c10ad3406d13e63bd990374a7a9fa2a2bd1f528d
dharmsen <dylandalton27@gmail.com>
2021-07-24 23:16:15 +0200

    removed coordinates, added some functionality

--- src/minesweeper/model/Grid.java
+++ src/minesweeper/model/Grid.java
@@ -4,18 +4,44 @@ import minesweeper.model.Cell;
 
 public class Grid {
     
-    private int x;
-    private int y;
     private Cell[][] matrix;
 
-    public Grid(int x, int y) {
-        this.x = x;
-        this.y = y;
+    public Grid() {
         // TODO add Cell to constructor?
     }
 
-    /** Generates a grid */
-    public void generateGrid() {
+    /** Calculate adjacent value for each cell in grid */
+    public void calculateAdjacent() {
+        for (int y=0; y<this.matrix.length; y++) {
+            for (int x=0; x<this.matrix[y].length; x++) {
+                // check/count surrounding cells for -1 
+            }
+        }
+    }
 
+    /** Set mines in grid */
+    public void setMines() {
+        for (int y=0; y<this.matrix.length; y++) {
+            for (int x=0; x<this.matrix[y].length; x++) {
+                if ((int)Math.random() * 10 == 0) { // 1 in 10 chance
+                    this.matrix[y][x].setAdjacent(-1);
+                }
+            }
+        }
+    }
+
+    /** Click cell */
+    public void clickCell(int xClicked, int yClicked) {
+        if (!this.matrix[yClicked][xClicked].getVisible()) {
+            this.matrix[yClicked][xClicked].setVisible(true); // if clicked, show adjacent
+        }
+    }
+
+    public Cell[][] getMatrix() {
+        return this.matrix;
+    }
+
+    public void setMatrix(Cell[][] matrix) {
+        this.matrix = matrix;
     }
 }
